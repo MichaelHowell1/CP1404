bc36f073e735db2f17133c8c34320a8c2881d3f3
Michael <michael.howell1@my.jcu.edu.au>
2022-09-25 03:37:26 +1000

    Get random score

--- Practicals/Prac 2/score.py
+++ Practicals/Prac 2/score.py
@@ -1,21 +1,34 @@
+import random
+
+
 def main():
 
     score = get_score()
+    if score > 100:
+        get_score()
+    elif score < 0:
+        get_score()
+    print_grade(score)
 
-    while score != "Q":
+    score = random_score()
+    print("Random score:", score)
+    print_grade(score)
 
-        if score > 100:
-            print("Please input a number between 0 and 100")
-        elif score >= 90:
-            print("Excellent ")
-        elif score >= 50:
-            print("Passable ")
-        elif score >= 0:
-            print("Bad ")
 
-        else:
-            print("Please input a number between 0 and 100")
-        score = float(input("Enter score: "))
+def print_grade(score):
+    if score >= 90:
+        print("Excellent ")
+
+    elif score >= 50:
+        print("Passable ")
+
+    else:
+        print("Bad ")
+
+
+def random_score():
+    score = random.randint(0, 100)
+    return score
 
 
 def get_score():
@@ -23,7 +36,6 @@ def get_score():
     print(MENU)
     score = float(input("Enter score: "))
     return score
-
 
 
 main()
