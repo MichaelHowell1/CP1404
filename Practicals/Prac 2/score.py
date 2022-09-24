8a14e51f742cbce7793ac44aea22def3911e6bfa
Michael <michael.howell1@my.jcu.edu.au>
2022-09-25 02:33:02 +1000

    Add random score

--- Practicals/Prac 2/score.py
+++ Practicals/Prac 2/score.py
@@ -1,17 +1,29 @@
-MENU = """Please input your score (between 0 and 100)"""
-print (MENU)
-score = float(input("Enter score: "))
-while score != "Q":
+def main():
 
-    if score > 100:
-        print("Please input a number between 0 and 100")
-    elif score >= 90:
-        print("Excellent ")
-    elif score >= 50:
-        print("Passable ")
-    elif score >=0:
-        print("Bad ")
+    score = get_score()
 
-    else:
-        print("Please input a number between 0 and 100")
+    while score != "Q":
+
+        if score > 100:
+            print("Please input a number between 0 and 100")
+        elif score >= 90:
+            print("Excellent ")
+        elif score >= 50:
+            print("Passable ")
+        elif score >= 0:
+            print("Bad ")
+
+        else:
+            print("Please input a number between 0 and 100")
+        score = float(input("Enter score: "))
+
+
+def get_score():
+    MENU = """Please input your score (between 0 and 100)"""
+    print(MENU)
     score = float(input("Enter score: "))
+    return score
+
+
+
+main()
