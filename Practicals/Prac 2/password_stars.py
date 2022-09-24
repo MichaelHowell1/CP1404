2146ffbe6ea92e7fc7b81d82ec8d1a965c11d215
Michael <michael.howell1@my.jcu.edu.au>
2022-09-25 01:54:42 +1000

    Refactor password check program to use functions

--- Practicals/Prac 2/password_stars.py
+++ Practicals/Prac 2/password_stars.py
@@ -1,12 +1,26 @@
-password = int(input("Please enter a 10 digit password: "))
-stars = password
-star_number = len(str(stars))
+def main():
 
-while star_number != "Q":
-    if star_number == 10:
-        print("**********")
-        star_number = "Q"
-    else:
-        password = input("Please enter a 10 digit password: ")
-        stars = password
-        star_number = len(str(stars))
+    star_number = get_password()
+
+    print_number(star_number)
+
+
+def print_number(star_number):
+    while star_number != "Q":
+        if star_number == 10:
+            print("**********")
+            star_number = "Q"
+        else:
+            password = input("Please enter a 10 digit password: ")
+            stars = password
+            star_number = len(str(stars))
+
+
+def get_password():
+    password = input("Please enter a 10 digit password: ")
+    stars = password
+    star_number = len(str(stars))
+    return star_number
+
+
+main()
