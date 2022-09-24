092a1126475999cbfa5acefb7957600d2f965bec
Michael <michael.howell1@my.jcu.edu.au>
2022-09-25 02:09:31 +1000

    Initial commit

---
+++ Practicals/Prac 2/score.py
@@ -0,0 +1,17 @@
+MENU = """Please input your score (between 0 and 100)"""
+print (MENU)
+score = float(input("Enter score: "))
+while score != "Q":
+
+    if score > 100:
+        print("Please input a number between 0 and 100")
+    elif score >= 90:
+        print("Excellent ")
+    elif score >= 50:
+        print("Passable ")
+    elif score >=0:
+        print("Bad ")
+
+    else:
+        print("Please input a number between 0 and 100")
+    score = float(input("Enter score: "))
