08fae34bdf72b0007e99c394dd606f77117e124a
Michael <michael.howell1@my.jcu.edu.au>
2022-09-29 21:18:18 +1000

    Completed task

--- Practicals/Prac 3/string_formatting.py
+++ Practicals/Prac 3/string_formatting.py
@@ -3,6 +3,9 @@ name = "Gibson L-5 CES"
 year = 1922
 cost = 16035.4
 
+"""convert cost to integer to remove decimal"""
+cost = int(cost)
+
 """ printing the year, name and cost using f-string formatting """
 print(f"{year} {name} for about ${cost}!")
 print()
