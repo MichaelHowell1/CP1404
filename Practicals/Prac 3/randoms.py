f97a5c0ccd369491ba08899b98c39de2d5b76c4b
Michael <michael.howell1@my.jcu.edu.au>
2022-09-29 21:31:31 +1000

    Produce random number

---
+++ Practicals/Prac 3/randoms.py
@@ -0,0 +1,44 @@
+"""
+Question 1:
+What did you see on line 1?     code = print(random.randint(5, 20))  # line 1
+6
+13
+12
+19
+12
+* the code was repeated 5 times
+
+What was the smallest number you could have seen, what was the largest?
+Smallest number could be 5, the largest could be 20.
+
+Question 2:
+What did you see on line 2?     code = print(random.randrange(3, 10, 2))  # line 2
+9
+7
+3
+5
+7
+* the code was repeated 5 times
+
+What was the smallest number you could have seen, what was the largest?
+The smallest number could be 3, the largest could be 9.
+
+Could line 2 have produced a 4?
+A 4 could not be produced as due to the number step, only odd numbers between 3 and 9 are possible.
+
+Question 3
+What did you see on line 3?   code = print(random.uniform(2.5, 5.5))  # line 3
+3.79726842173829
+4.667228062559547
+2.8038065298545654
+2.7667004418200087
+4.851036210069926
+* the code was repeated 5 times
+
+What was the smallest number you could have seen, what was the largest?
+The smallest could be 2.5 and the largest could have been 5.5
+
+
+
+
+"""
