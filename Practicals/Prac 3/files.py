75ae41062f4494e09199ba6bdb305a98ecdff910
Michael <michael.howell1@my.jcu.edu.au>
2022-10-01 00:24:44 +1000

    Completed task

---
+++ Practicals/Prac 3/files.py
@@ -0,0 +1,29 @@
+""" Writing the name to file"""
+name = str(input("Please enter your name: "))
+output_file = "name.txt"
+out_file = open('name.txt', 'w')
+print(f"{name}", file=out_file)
+out_file.close()
+
+"""Reading the name from the file"""
+in_file = open('name.txt', 'r')
+name = in_file.readline()
+print(f"Your name is {name}")
+in_file.close()
+
+"""Reading the two numbers from file"""
+in_file = open('numbers.txt', 'r')
+number1 = int(in_file.readline())
+number2 = int(in_file.readline())
+total_number = number1 + number2
+print(f"Number 1 + Number 2 = {total_number}")
+in_file.close()
+
+"""Reading all numbers from file"""
+in_file = open('numbers.txt', 'r')
+total_number = 0
+for line_number in in_file:
+    number = int(line_number)
+    total_number = total_number + number
+print(f"The total addition of the numbers in numbers.txt file = {total_number}")
+in_file.close()
