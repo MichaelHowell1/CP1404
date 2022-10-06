0fe9e478175ebdcf230cd997ebc40b1d98558d28
Michael <michael.howell1@my.jcu.edu.au>
2022-10-06 14:31:48 +1000

    Prevent repeated pick in line

--- Practicals/Prac 4/quick_picks.py
+++ Practicals/Prac 4/quick_picks.py
@@ -1,6 +1,6 @@
 """
 CP1404 Prac 4 - Quick Picks
-Simulate random "quick picks"
+Simulate random "quick picks", 6 per line and user input for number of lines.
 """
 
 import random
@@ -8,11 +8,10 @@ import random
 number_of_picks = int(input("How many quick picks? "))
 picks_per_line = 6
 
-for temp in range(number_of_picks):
+for picks_line in range(number_of_picks):
     pick_numbers = []
-    for temp in range(picks_per_line):
+    for picks in range(picks_per_line):
         number = random.randint(1, 45)
         pick_numbers.append(number)
     pick_numbers.sort()
     print(f" ".join("{:2}".format(number) for number in pick_numbers))
-print(pick_numbers)
