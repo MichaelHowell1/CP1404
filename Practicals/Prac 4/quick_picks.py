35940159cfbfb4b1f4ca879e641f4f539caace20
Michael <michael.howell1@my.jcu.edu.au>
2022-10-06 14:33:25 +1000

    Completed task

--- Practicals/Prac 4/quick_picks.py
+++ Practicals/Prac 4/quick_picks.py
@@ -12,6 +12,8 @@ for picks_line in range(number_of_picks):
     pick_numbers = []
     for picks in range(picks_per_line):
         number = random.randint(1, 45)
+        while number in pick_numbers:
+            number = random.randint(1, 45)
         pick_numbers.append(number)
     pick_numbers.sort()
     print(f" ".join("{:2}".format(number) for number in pick_numbers))
