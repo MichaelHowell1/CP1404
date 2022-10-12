7f5d32d04f138e66d75865c81e24036c25039370
Michael <michael.howell1@my.jcu.edu.au>
2022-10-12 19:20:25 +1000

    Add appropriate comments

--- Practicals/Prac 4/list_comprehensions.py
+++ Practicals/Prac 4/list_comprehensions.py
@@ -29,24 +29,24 @@ print(a_names)
 # 'Ada Alan Angel Bob Jimi'
 print(" ".join(sorted(names)))
 
-"""
-TODO section below
-"""
 
-"""Print the full names in lower case"""
+# Section 2 - Assigned tasks
+print("\nSection 2\n", "-"*20)
+
+# Print the full names in lower case
 lowercase_full_names = [full_names.lower() for full_names in full_names]
 print(lowercase_full_names)
 
-"""Convert the "almost numbers" to integers"""
+# Convert the "almost numbers" list to integers
 almost_numbers = ['0', '10', '21', '3', '-7', '88', '9']
 numbers = [int(almost_number) for almost_number in almost_numbers]
 print(numbers)
 
-"""Finds numbers greater than 9 from the almost number list"""
+# Finds numbers greater than 9 from the almost number list
 numbers_above_nine = [number for number in numbers if number > 9]
 print(numbers_above_nine)
 
-"""Finds names above 11 characters and prints the lastnames as a string """
+# Finds names longer than 11 characters and prints the lastnames
 names_above_eleven = [full_names for full_names in full_names if len(full_names) > 11]
 last_names = [names_above_eleven.split()[1] for names_above_eleven in names_above_eleven]
 print(", ".join(sorted(last_names)))
