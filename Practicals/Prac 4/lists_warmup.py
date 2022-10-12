5855f56ee7dca59388b9a52d3726ebf29b06059d
Michael <michael.howell1@my.jcu.edu.au>
2022-10-12 19:32:41 +1000

    Change docstring to comments

--- Practicals/Prac 4/lists_warmup.py
+++ Practicals/Prac 4/lists_warmup.py
@@ -1,17 +1,19 @@
-""" CP1404 Prac 4 - Lists Warmup
-Warm-up tasks using lists"""
+"""
+CP1404 Prac 4 - Lists Warmup
+Warm-up tasks using lists
+"""
 
 numbers = [3, 1, 4, 1, 5, 9, 2]
-""" Question answers
-numbers[0]          = [3]
-numbers[-1]         = [1]
-numbers[3]          = [4]
-numbers[:-1]        = [3, 1, 4, 1, 5, 9]
-numbers[3:4]        = [1]
-5 in numbers        = [True]
-7 in numbers        = [False]
-"3" in numbers      = [False]
-numbers + [6, 5, 3] = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]"""
+# Question answers
+# numbers[0]          = [3]
+# numbers[-1]         = [1]
+# numbers[3]          = [4]
+# numbers[:-1]        = [3, 1, 4, 1, 5, 9]
+# numbers[3:4]        = [1]
+# 5 in numbers        = [True]
+# 7 in numbers        = [False]
+# "3" in numbers      = [False]
+# numbers + [6, 5, 3] = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]"""
 
 numbers[0] = "ten"
 numbers[-1] = 1
