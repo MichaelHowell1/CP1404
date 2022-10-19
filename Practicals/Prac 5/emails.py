501b223ad2d49f06d3f3c6fb309684cc763af002
Michael <michael.howell1@my.jcu.edu.au>
2022-10-19 17:41:01 +1000

    Complete prac 5 email exercise

--- Practicals/Prac 5/emails.py
+++ Practicals/Prac 5/emails.py
@@ -1,23 +1,39 @@
 """
 CP1404 Prac 5 - Emails
-Add unique emails to dictionary
-Estimate: 1hr
-Actual
+Add unique emails to dictionary with their name
+
+Estimate: 1 hour
+Actual: 48 minutes
 """
 
 
 def main():
-    """ Dictionary of emails to names."""
+    """Dictionary of emails to names."""
     email_to_name = {}
     email = input("Email: ")
-    name = extract_name(email)
+
+    while email != "":
+        name = extract_name(email)
+        email_to_name[email] = check_name(name)
+        email = input("Email: ")
+
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
 
 
 def extract_name(email):
-    """extract name from email"""
+    """Extract name from email"""
     username = email.split("@")[0]
     name_parts = username.split(".")
     name = " ".join(name_parts).title()
+    return name
+
+
+def check_name(name):
+    """Check if name is correct with user"""
+    check = input(f"Is your name {name}? (Y/N)").upper()
+    if check != "Y" and check != "":
+        name = input("Name: ").title()
     return name
 
 
