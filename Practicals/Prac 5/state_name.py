e77aec164777e37c05f25707a382d7c071427921
Michael <michael.howell1@my.jcu.edu.au>
2022-10-19 13:15:56 +1000

    Add formatting

--- Practicals/Prac 5/state_name.py
+++ Practicals/Prac 5/state_name.py
@@ -1,10 +1,8 @@
 """
-CP1404/CP5632 Practical
-State names in a dictionary
-File needs reformatting
+CP1404 Prac 5 - Michael Howell
+Print state names from abbreviation
 """
 
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
@@ -12,7 +10,7 @@ print(CODE_TO_NAME)
 state_code = input("Enter short state: ").upper()
 while state_code != "":
     if state_code in CODE_TO_NAME:
-        print(f"{state_code} is {CODE_TO_NAME[state_code]}")
+        print(f"{state_code:3} is {CODE_TO_NAME[state_code]}")
     else:
         print("Invalid short state")
     state_code = input("Enter short state: ").upper()
