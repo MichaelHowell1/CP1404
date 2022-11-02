dc6e73fef8b21b3ea0a49e2b844192d17aeae12e
Michael <michael.howell1@my.jcu.edu.au>
2022-11-02 17:33:36 +1000

    Add working largest word length parameter

--- Practicals/prac_06/guitars.py
+++ Practicals/prac_06/guitars.py
@@ -3,16 +3,16 @@ CP1404 Prac 6 - Guitars
 
 
 Estimate: 30 minutes
-Actual:
+Actual: 2hrs
 """
 from prac_06.guitar import Guitar
-from operator import attrgetter
+
 
 def main():
-    """ Notes"""
+
 
     guitars = []
-    """    guitar_name = input("Name: ")
+    guitar_name = input("Name: ")
     while guitar_name != "":
 
         guitar_year = input("Year: ")
@@ -20,19 +20,20 @@ def main():
         current_guitar = Guitar(guitar_name, guitar_year, guitar_cost)
         guitars.append(current_guitar)
         print(f"{guitar_name} ({guitar_year}) : {guitar_cost} added")
-        guitar_name = input("Name: ")"""
-    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
-    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+        guitar_name = input("Name: ")
+    """    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))"""
 
-    largest_word = max(guitars, key=attrgetter("name"))
-    print(largest_word)
-    largest_word_length = len(largest_word.name)+1
-    print(largest_word_length)
+    largest_word_length = 0
+    for guitar in guitars:
+        if largest_word_length < len(guitar.name):
+            largest_word_length = len(guitar.name)
 
-    for i, guitar in enumerate(guitars, 1):
+    for guitar_number, guitar in enumerate(guitars, 1):
 
         vintage_string = "(vintage)" if guitar.is_vintage() is True else ""
-        print(f"Guitar {i}: {guitar.name:>{largest_word_length}} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+        print(f"Guitar {guitar_number}: {guitar.name:>{largest_word_length}} ({guitar.year}), worth "
+              f"${guitar.cost:10,.2f} {vintage_string}")
 
 
 
