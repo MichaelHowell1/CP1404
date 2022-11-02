3997f4832b6c9644bc9f205bc9e474b0ab3926ae
Michael <michael.howell1@my.jcu.edu.au>
2022-11-02 17:08:31 +1000

    Print guitars

--- Practicals/prac_06/guitars.py
+++ Practicals/prac_06/guitars.py
@@ -6,18 +6,13 @@ Estimate: 30 minutes
 Actual:
 """
 from prac_06.guitar import Guitar
-
+from operator import attrgetter
 
 def main():
-
-    """ Notes
-    category = "adult" if age >= 18 else "child"
-    print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
-    for i, guitar in enumerate(guitars, 1):
-        # do something with i (the index) and guitar (the element)"""
+    """ Notes"""
 
     guitars = []
-    guitar_name = input("Name: ")
+    """    guitar_name = input("Name: ")
     while guitar_name != "":
 
         guitar_year = input("Year: ")
@@ -25,7 +20,18 @@ def main():
         current_guitar = Guitar(guitar_name, guitar_year, guitar_cost)
         guitars.append(current_guitar)
         print(f"{guitar_name} ({guitar_year}) : {guitar_cost} added")
-        guitar_name = input("Name: ")
+        guitar_name = input("Name: ")"""
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+
+
+    for i, guitar in enumerate(guitars, 1):
+
+        vintage_string = "(vintage)" if guitar.is_vintage() is True else ""
+        print(f"Guitar {i}: {guitar.name:>{largest_word_length}} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+
+
 
 
 main()
