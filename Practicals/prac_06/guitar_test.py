1d7bd7535e19b2308d6a771b27edba5fd7d59d7f
Michael <michael.howell1@my.jcu.edu.au>
2022-11-02 14:46:56 +1000

    Add complete Guitar class test

--- Practicals/prac_06/guitar_test.py
+++ Practicals/prac_06/guitar_test.py
@@ -7,12 +7,16 @@ Actual:
 """
 from prac_06.guitar import Guitar
 
+
 def main():
 
-    gibson = Guitar("Gibson_L_-_5_CES", 1922, 16035.40)
+    gibson = Guitar("Gibson L-5 CES", 1922, 16035.40)
     another_guitar = Guitar("Another Guitar", 2013)
-    print(gibson)
-    print(another_guitar)
+    print(f"{gibson.name} get_age() - Expected 100. Got {gibson.get_age()}")
+    print(f"{another_guitar.name} get_age() - Expected 100. Got {another_guitar.get_age()}")
+
+    print(f"{gibson.name} is_vintage() - Expected True. Got {gibson.is_vintage()}")
+    print(f"{another_guitar.name} is_vintage() - Expected False. Got {another_guitar.is_vintage()}")
 
 
 main()
