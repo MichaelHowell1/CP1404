57044aa2138682c637876e6a8972edc2493dea31
Michael <michael.howell1@my.jcu.edu.au>
2022-11-02 13:39:55 +1000

    Add __str__ method to produce string of data

--- Practicals/prac_06/programming_language.py
+++ Practicals/prac_06/programming_language.py
@@ -9,7 +9,14 @@ Actual:
 class ProgrammingLanguage:
     """"""
 
-    def __init__(self):
+    def __init__(self, name="", typing="", reflection="", year=""):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    """def is_dynamic(self):"""
 
 
-    def is_dynamic(self):
+    def __str__(self):
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
