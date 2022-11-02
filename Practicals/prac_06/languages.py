67547581589a863f2f12bdcb6a143ca19798d8ee
Michael <michael.howell1@my.jcu.edu.au>
2022-11-02 12:54:46 +1000

    Create simple program in languages.py

--- Practicals/prac_06/languages.py
+++ Practicals/prac_06/languages.py
@@ -5,3 +5,15 @@ CP1404 Prac 6 - Languages
 Estimate: 30 minutes
 Actual:
 """
+
+from prac_06.programming_language import ProgrammingLanguage
+
+
+def main():
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(python)
+
+
+main()
--- Practicals/prac_06/programming_language.py
+++ Practicals/prac_06/programming_language.py
@@ -5,3 +5,11 @@ CP1404 Prac 6 - Programming Language
 Estimate: 30 minutes
 Actual:
 """
+
+class ProgrammingLanguage:
+    """"""
+
+    def __init__(self):
+
+
+    def is_dynamic(self):
