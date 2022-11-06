6895a88ab4b4fe8c88e4b4ffe429ad255ea720c0
Michael <michael.howell1@my.jcu.edu.au>
2022-11-07 06:09:00 +1000

    Add additional language data (java)

--- Practicals/prac_07/programming_language.py
+++ Practicals/prac_07/programming_language.py
@@ -28,8 +28,11 @@ def run_tests():
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    java = ProgrammingLanguage("Java", "Static", True, 1995)
+    # Java data from: http://www.jvoegele.com/software/langcomp.html
+    # and year from: https://www.britannica.com/technology/Java-computer-programming-language
 
-    languages = [ruby, python, visual_basic]
+    languages = [ruby, python, visual_basic, java]
     print(python)
 
     print("The dynamically typed languages are:")
