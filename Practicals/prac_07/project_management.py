9be1a48c4e9e2d13e5e63ec90b9bb08b4f0208c8
Michael <michael.howell1@my.jcu.edu.au>
2022-11-10 09:17:48 +1000

    Add menu

--- Practicals/prac_07/project_management.py
+++ Practicals/prac_07/project_management.py
@@ -2,3 +2,14 @@
 CP1404 Prac 7 - Project Management
 
 """
+
+
+def main():
+    menu = """Menu:
+L - Load projects
+S - Save projects 
+D - Display projects
+F - Filter projects by date
+A - Add new project
+U - Update project
+>>> """
