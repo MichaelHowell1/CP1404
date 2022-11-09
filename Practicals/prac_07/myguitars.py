5a85e81e31b335278da46c3408b1666ddb2571c2
Michael <michael.howell1@my.jcu.edu.au>
2022-11-10 09:11:17 +1000

    Save guitars to file

--- Practicals/prac_07/myguitars.py
+++ Practicals/prac_07/myguitars.py
@@ -4,26 +4,31 @@ CP1404 Prac 7 - My Guitars
 """
 
 from prac_06.guitar import Guitar
-
+filename = 'guitars.csv'
 
 def main():
     """ """
     guitars = get_guitar_data()
+    display_guitars(guitars)
     get_user_guitar(guitars)
     guitars.sort()
-    for guitar in guitars:
-        print(guitar)
-
+    save_guitars(guitars)
+    print(f"Guitars have been saved")
 
 def get_guitar_data():
     """ """
     guitars = []
-    with open('guitars.csv', 'r') as in_file:
+    with open(filename, 'r') as in_file:
         for line in in_file:
             parts = line.strip().split(',')
             guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
             guitars.append(guitar)
     return guitars
+
+
+def display_guitars(guitars):
+    for guitar in guitars:
+        print(guitar)
 
 
 def get_user_guitar(guitars):
@@ -36,6 +41,14 @@ def get_user_guitar(guitars):
         guitars.append(current_guitar)
         print(f"{guitar_name} ({guitar_year}) : {guitar_cost} added")
         guitar_name = input("Name: ")
+
+
+def save_guitars(guitars):
+    """ """
+    with open(filename, 'w') as out_file:
+        for guitar in guitars:
+            print(f"{guitar.name},{guitar.year},{guitar.cost}", file=out_file)
+
 
 
 main()
