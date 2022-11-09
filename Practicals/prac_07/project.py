79db7bd8dc41c96d846fcb0e402d349875c9476c
Michael <michael.howell1@my.jcu.edu.au>
2022-11-10 09:49:33 +1000

    Add readable string output

--- Practicals/prac_07/project.py
+++ Practicals/prac_07/project.py
@@ -13,3 +13,7 @@ class Project:
         self.cost_estimate = cost_estimate
         self.completion_percentage = completion_percentage
 
+    def __str__(self):
+        """Change class output into readable string."""
+        return f"{self.name}, start: {self.start_date}, priority {self.priority}, estimate: ${self.cost_estimate}, " \
+               f"completion: {self.completion_percentage}% "
