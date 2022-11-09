d92ca0f3c433158ad7e669e1ca5e1a4dc58b0008
Michael <michael.howell1@my.jcu.edu.au>
2022-11-09 21:07:32 +1000

    Add get_guitar_data function to read guitars.csv file

--- Practicals/prac_07/myguitars.py
+++ Practicals/prac_07/myguitars.py
@@ -2,3 +2,25 @@
 CP1404 Prac 7 - My Guitars
 
 """
+
+from prac_06.guitar import Guitar
+
+
+def main():
+    """ """
+    guitars = get_guitar_data()
+
+
+def get_guitar_data():
+    """ """
+    guitars = []
+    with open('guitars.csv', 'r') as in_file:
+        for line in in_file:
+            parts = line.strip().split(',')
+            guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
+            guitars.append(guitar)
+
+    return guitars
+
+
+main()
