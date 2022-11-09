1a4190684efa381fa8ae848ea55dafbc9e001379
Michael <michael.howell1@my.jcu.edu.au>
2022-11-10 09:35:32 +1000

    Create a project instance

--- Practicals/prac_07/project.py
+++ Practicals/prac_07/project.py
@@ -2,3 +2,14 @@
 CP1404 Prac 7 - Project
 
 """
+
+
+class Project:
+    def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
+        """Create a project"""
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_percentage
+
