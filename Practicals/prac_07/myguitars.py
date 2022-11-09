fbf4df0ad719a0fb033684f37ac816b85a5283f5
Michael <michael.howell1@my.jcu.edu.au>
2022-11-10 09:00:12 +1000

    Add get_user_guitar function to add user's guitars to the list

--- Practicals/prac_07/myguitars.py
+++ Practicals/prac_07/myguitars.py
@@ -9,6 +9,7 @@ from prac_06.guitar import Guitar
 def main():
     """ """
     guitars = get_guitar_data()
+    get_user_guitar(guitars)
     guitars.sort()
     for guitar in guitars:
         print(guitar)
@@ -22,8 +23,19 @@ def get_guitar_data():
             parts = line.strip().split(',')
             guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
             guitars.append(guitar)
-
     return guitars
+
+
+def get_user_guitar(guitars):
+    """ """
+    guitar_name = input("Name: ")
+    while guitar_name != "":
+        guitar_year = int(input("Year: "))
+        guitar_cost = float(input("Cost: $"))
+        current_guitar = Guitar(guitar_name, guitar_year, guitar_cost)
+        guitars.append(current_guitar)
+        print(f"{guitar_name} ({guitar_year}) : {guitar_cost} added")
+        guitar_name = input("Name: ")
 
 
 main()
