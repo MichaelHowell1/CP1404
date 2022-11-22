2fe973a296447b269561190b651332e76d37682d
Michael <michael.howell1@my.jcu.edu.au>
2022-11-22 23:59:37 +1000

    Add class variable price_per_km to the Taxi class

--- Practicals/prac_09/taxi_test.py
+++ Practicals/prac_09/taxi_test.py
@@ -9,7 +9,7 @@ from prac_09.taxi import Taxi
 def main():
     """Client test code for a taxi"""
     # Initial fare
-    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi = Taxi("Prius 1", 100)
     my_taxi.drive(40)
     print(f"{my_taxi}\nCurrent fare: ${my_taxi.get_fare()}")
     # Start a new fare
