dd560c7bb0223ef219e0080f80905727b9e76d0e
Michael <michael.howell1@my.jcu.edu.au>
2022-11-23 14:04:24 +1000

    Adjust string to match example output, ie add space before "plus" and make sel.flagfall show cents properly

--- Practicals/prac_09/silver_service_taxi.py
+++ Practicals/prac_09/silver_service_taxi.py
@@ -19,7 +19,7 @@ class SilverServiceTaxi(Taxi):
 
     def __str__(self):
         """Return a string for SilverServiceTaxi, based on taxi + flagfall."""
-        return f"{super().__str__()}plus flagfall of ${self.flagfall}"
+        return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
 
     def get_fare(self):
         """Get the current fare."""
