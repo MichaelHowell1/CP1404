33b2c023d88c0a951bc75a75b05fa39157c4aa9c
Michael <michael.howell1@my.jcu.edu.au>
2022-11-23 19:13:14 +1000

    Complete menu choice D

--- Practicals/prac_09/taxi_simulator.py
+++ Practicals/prac_09/taxi_simulator.py
@@ -33,8 +33,12 @@ def main():
             if current_taxi is None:
                 print("You need to choose a taxi before you can drive")
             else:
-                print()
-
+                current_taxi.start_fare()
+                drive_distance = float(input("Drive how far? "))
+                current_taxi.drive(drive_distance)
+                trip_cost = current_taxi.get_fare()
+                print(f"Your {current_taxi.name} trip cost you ${trip_cost}")
+                total_bill += trip_cost
         else:
             print("Invalid choice")
 
