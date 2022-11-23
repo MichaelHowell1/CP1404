c0ced0714d1d669838fe288117858395cb8816da
Michael <michael.howell1@my.jcu.edu.au>
2022-11-23 13:49:21 +1000

    Add class: silver service taxi, prior to testing client code

---
+++ Practicals/prac_09/silver_service_taxi.py
@@ -0,0 +1,26 @@
+"""
+CP1404 Prac 9 - Silver Service Taxi Class
+A class for a Silver Service Taxi, derived from parent class taxi
+"""
+
+from prac_09.taxi import Taxi
+
+
+class SilverServiceTaxi(Taxi):
+    """Specialised version of a Taxi that includes fanciness and a flagfall."""
+
+    flagfall = 4.5  # Extra charge for each new fare
+
+    def __init__(self, name, fuel, fanciness):
+        """Initialise a Silver Service Taxi instance, based on parent class Taxi."""
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km *= fanciness
+
+    def __str__(self):
+        """Return a string for SilverServiceTaxi, based on taxi + flagfall."""
+        return f"{super().__str__()}plus flagfall of ${self.flagfall}"
+
+    def get_fare(self):
+        """Get the current fare."""
+        return super().get_fare() + self.flagfall
